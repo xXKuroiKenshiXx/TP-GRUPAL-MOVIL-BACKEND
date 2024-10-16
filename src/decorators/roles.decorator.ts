980f597a1018432eeddf50b5dddf49e32f96d2e6
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2024-10-16 19:44:34 -0300

    Delete src/decorators directory

--- src/decorators/roles.decorator.ts
+++
@@ -1,8 +0,0 @@
-/*
-https://docs.nestjs.com/openapi/decorators#decorators
-*/
-
-import { Reflector } from '@nestjs/core';
-import { RolesEnum } from 'src/enums/roles.enum';
-
-export const Roles = Reflector.createDecorator<RolesEnum[]>( );
